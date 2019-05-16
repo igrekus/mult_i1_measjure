0f2ab2e90077a709f99a491a1b0633920ec4ac12
igrekus <upload234@mail.ru>
2019-05-16 17:09:04 +0300

    WIP: add instrument controller widget
    generates GUI for the required instruments from the passed instrument controller

---
+++ instrumentcontrollerwidget.py
@@ -0,0 +1,42 @@
+from PyQt5 import uic
+from PyQt5.QtCore import pyqtSlot, pyqtSignal
+from PyQt5.QtWidgets import QWidget
+
+from instrumentwidget import InstrumentWidget
+
+
+class InstrumentControllerWidget(QWidget):
+
+    connected = pyqtSignal()
+
+    def __init__(self, parent=None, controller=None):
+        super().__init__(parent=parent)
+
+        self._ui = uic.loadUi('D:\work\python\multiplier-i1\instrumentcontrollerwidget.ui', self)
+        self._controller = controller
+
+        self._widgets = {
+            k: InstrumentWidget(parent=self, title=f'{k}', addr=f'{v.addr}')
+            for k, v in self._controller.requiredInstruments.items()
+        }
+
+        self._setupUi()
+
+    def _setupUi(self):
+        for i, iw in enumerate(self._widgets.items()):
+            self._ui.layInstruments.insertWidget(i, iw[1])
+
+    @pyqtSlot()
+    def on_btnConnect_clicked(self):
+        print('connect')
+
+        if not self._controller.connect({k: w.address for k, w in self._widgets.items()}):
+            print('connect error check connection')
+            return
+
+        self.connected.emit()
+        for w, s in zip(self._widgets.values(), self._controller.status):
+            w.status = s
+
+
+
