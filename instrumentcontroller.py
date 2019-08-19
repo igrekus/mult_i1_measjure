8167a0773e8880bb46af511268b74a1e7c63835a
igrekus <upload234@mail.ru>
2019-08-19 16:55:39 +0300

    FIX: add E4483C as available Gen

--- instrumentcontroller.py
+++ instrumentcontroller.py
@@ -1,4 +1,4 @@
-﻿import random
+import random
 import time
 import pandas
 import visa
@@ -50,7 +50,7 @@ class InstrumentFactory:
 class GeneratorFactory(InstrumentFactory):
     def __init__(self, addr):
         super().__init__(addr=addr, label='Генератор')
-        self.applicable = ['N5183A', 'N5181B']
+        self.applicable = ['N5183A', 'N5181B', 'E4438C']
     def from_address(self):
         if mock_enabled:
             return AgilentN5183A(self.addr, '1,N5183A mock,1', AgilentN5183AMock())
